0e30aaa7af18476d1161f2523e88b25ec9c1861b
iviooThong <kitipot2538@gmail.com>
2018-02-05 13:10:49 +0700

    edit method program_department

--- app/Http/Controllers/program_department/ProgramDepartmentController.php
+++ app/Http/Controllers/program_department/ProgramDepartmentController.php
@@ -8,42 +8,39 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class ProgramDepartmentController extends BaseController
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+      //
+  }
 
-    private $response = array('status' => 1, 'message' => 'success');
+  private $response = array('status' => 1, 'message' => 'success');
 
-    public function program_department(){
-      $results = ProgramDepartment::all();
-      return response()->json($results);
-    }
+  public function program_department(){
+    $results = ProgramDepartment::all();
+    return response()->json($results);
+  }
 
-    public function create(Request $request){
-      $result = new ProgramDepartment;
-      // $result->id = $request->id;
-      $result->caption = $request->caption;
-      $result->save();
-      return response()->json($this->response);
-    }
+  // public function create(Request $request){
+  //   $result = new ProgramDepartment;
+  //   $result->caption = $request->caption;
+  //   $result->save();
+  //   return response()->json($this->response);
+  // }
 
-    public function edit(Request $request){
-      $result = ProgramDepartment::find($request->id);
-      $result->caption = $request->caption;
-      $result->save();
-      return response()->json($this->response);
-    }
+  // public function edit(Request $request){
+  //   $result = ProgramDepartment::find($request->id);
+  //   $result->caption = $request->caption;
+  //   $result->save();
+  //   return response()->json($this->response);
+  // }
 
-    public function delete(Request $request){
-      $result = ProgramDepartment::find($request->id);
-      $result->caption = $request->caption;      
-      $result->save();
-      return response()->json($this->response);
-    }
+  // public function delete(Request $request){
+  //   $result->save();
+  //   return response()->json($this->response);
+  // }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,8 @@ $router->get('/key', function () use ($router) {
     return str_random(32);
 });
 
+// $router->get('/program_depa/{id:[0-9]+}', 'program_department\ProgramDepartmentController@program_department');
 $router->get('/program_depa', 'program_department\ProgramDepartmentController@program_department');
-$router->post('/program_depa/create', 'program_department\ProgramDepartmentController@create');
-$router->post('/program_depa/edit', 'program_department\ProgramDepartmentController@edit');
-$router->post('/program_depa/delete', 'program_department\ProgramDepartmentController@delete');
+// $router->post('/program_depa/create', 'program_department\ProgramDepartmentController@create');
+// $router->post('/program_depa/edit', 'program_department\ProgramDepartmentController@edit');
+// $router->delete('/program_depa/delete', 'program_department\ProgramDepartmentController@delete');
