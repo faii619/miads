44f43dec57b1aed9347bcad12855d9d5343cc352
faii619 <faiifylmus@gmail.com>
2018-02-06 16:58:53 +0700

    edit function create for AlumniController

--- app/Http/Controllers/alumni/AlumniController.php
+++ app/Http/Controllers/alumni/AlumniController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers\alumni;
 
 use App\Models\person\Person;
 use App\Models\address\Address;
+use App\Models\file\File;
+use App\Models\alumni\Alumni;
+use App\Models\user_login\UserLogin;
+use App\Models\user_login_user_role\UserLoginUserRole;
+use App\Models\career\Career;
 use App\Models\program_participant\ProgramParticipant;
 use Illuminate\Http\Request;
+use Illuminate\Hashing\BcryptHasher;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class AlumniController extends BaseController
@@ -61,7 +67,7 @@ class AlumniController extends BaseController
     if (count($result) > 0) {
       $result[0]['homeAddress'] = ($result[0]['homeAddressId'] > 0 && $result[0]['homeAddressId'] != NULL) ? $this->get_person_address($result[0]['homeAddressId']) : $this->text_status ;
       $result[0]['officeAddress'] = ($result[0]['officeAddressId'] > 0 && $result[0]['officeAddressId'] != NULL) ? $this->get_person_address($result[0]['officeAddressId']) : $this->text_status ;
-      $result[0]['officeContactAddress'] = ($result[0]['isPreferOfficeContact'] > 0 && $result[0]['isPreferOfficeContact'] != NULL) ? $this->get_person_address($result[0]['isPreferOfficeContact']) : $this->text_status ;
+      $result[0]['officeContactAddress'] = ($result[0]['isPreferOfficeContact'] = 0) ? $result[0]['homeAddress'] : $result[0]['officeAddress'] ;
       $result[0]['program'] = $this->get_person_program($result[0]['id']);
     }
 
@@ -87,80 +93,87 @@ class AlumniController extends BaseController
 
   public function create(Request $request)
   {
+    $personCode = $request->code;
+
     $instanceFile = new File;
-    $instanceFile->fileName = $request->;
-    $instanceFile->fileSize = $request->;
+    $instanceFile->fileName = $request->imageName;
+    $instanceFile->fileSize = $request->imageSize;
     $instanceFile->save();
-    
-    $instancePerson = new Person;
-    $instancePerson->personTitleId = $request->;
-    $instancePerson->name = $request->;
-    $instancePerson->birthDate = $request->;
-    $instancePerson->email = $request->;
-    $instancePerson->otherEmails = $request->;
-    $instancePerson->photoFileId = $request->;//tb file->PK
-    $instancePerson->homeAddressId = $request->;
-    $instancePerson->officeAddressId = $request->;
-    $instancePerson->isPreferOfficeContact = $request->;
-    $instancePerson->gender = $request->;
-    $instancePerson->nationalityAddressCountryId = $request->;
-    $instancePerson->save();
-    
-    $instanceAlumni = new Alumni;
-    $instanceAlumni->code = $request->;
-    $instanceAlumni->personId = $request->;
-    $instanceAlumni->save();
+    $fileId = $instanceFile->id;
     
     $instanceHomeAddress = new address;
-    $instanceHomeAddress->streetAddress = $request->;
-    $instanceHomeAddress->city = $request->;
-    $instanceHomeAddress->province = $request->;
-    $instanceHomeAddress->addressCountryId = $request->;
-    $instanceHomeAddress->zip = $request->;
-    $instanceHomeAddress->tel = $request->;
-    $instanceHomeAddress->fax = $request->;
-    $instanceHomeAddress->mobile = $request->;
+    $instanceHomeAddress->streetAddress = $request->homeStreetAddress;
+    $instanceHomeAddress->city = $request->homeCity;
+    $instanceHomeAddress->province = $request->homeProvince;
+    $instanceHomeAddress->addressCountryId = $request->homeCountry;
+    $instanceHomeAddress->zip = $request->homeZip;
+    $instanceHomeAddress->tel = $request->homeTel;
+    $instanceHomeAddress->fax = $request->homeFax;
+    $instanceHomeAddress->mobile = $request->homeMobile;
     $instanceHomeAddress->save();
+    $homeId = $instanceHomeAddress->id;
     
     $instanceOfficeAddress = new address;
-    $instanceOfficeAddress->streetAddress = $request->;
-    $instanceOfficeAddress->city = $request->;
-    $instanceOfficeAddress->province = $request->;
-    $instanceOfficeAddress->addressCountryId = $request->;
-    $instanceOfficeAddress->zip = $request->;
-    $instanceOfficeAddress->tel = $request->;
-    $instanceOfficeAddress->fax = $request->;
-    $instanceOfficeAddress->mobile = $request->;
+    $instanceOfficeAddress->streetAddress = $request->officeStreetAddress;
+    $instanceOfficeAddress->city = $request->officeCity;
+    $instanceOfficeAddress->province = $request->officeProvince;
+    $instanceOfficeAddress->addressCountryId = $request->officeCountry;
+    $instanceOfficeAddress->zip = $request->officeZip;
+    $instanceOfficeAddress->tel = $request->officeTel;
+    $instanceOfficeAddress->fax = $request->officeFax;
+    $instanceOfficeAddress->mobile = $request->officeMobile;
     $instanceOfficeAddress->save();
+    $officeId = $instanceOfficeAddress->id;
     
-    $instanceCareer = new career;
-    $instanceCareer->position = $request->;
-    $instanceCareer->startYear = $request->;
-    $instanceCareer->areaOfExpertise = $request->;
-    $instanceCareer->govMinistryName = $request->;
-    $instanceCareer->govDepartmentName = $request->;
-    $instanceCareer->organizationName = $request->;
-    $instanceCareer->organizationDepartment = $request->;
-    $instanceCareer->universityName = $request->;
-    $instanceCareer->universityDepartment = $request->;
-    $instanceCareer->careerOrganizationTypeId = $request->;
-    $instanceCareer->personId = $request->;
-    $instanceCareer->division = $request->;
+    $instancePerson = new Person;
+    $instancePerson->personTitleId = $request->title;
+    $instancePerson->name = $request->name;
+    $instancePerson->birthDate = $request->birthday;
+    $instancePerson->email = $request->email;
+    $instancePerson->otherEmails = $request->otherEmails;
+    $instancePerson->photoFileId = $fileId;
+    $instancePerson->homeAddressId = $homeId;
+    $instancePerson->officeAddressId = $officeId;
+    $instancePerson->isPreferOfficeContact = $request->ContactAddress;
+    $instancePerson->gender = $request->gender;
+    $instancePerson->nationalityAddressCountryId = $request->nationCountry;
+    $instancePerson->save();
+    $personId = $instancePerson->id;
+    
+    $instanceAlumni = new Alumni;
+    $instanceAlumni->code = $personCode;
+    $instanceAlumni->personId = $personId;
+    $instanceAlumni->save();
+    
+    $instanceCareer = new Career;
+    $instanceCareer->position = $request->careerPosition;
+    $instanceCareer->startYear = $request->careerStartYear;
+    $instanceCareer->areaOfExpertise = $request->careerExpertise;
+    $instanceCareer->govMinistryName = $request->careerMinistry;
+    $instanceCareer->govDepartmentName = $request->careerDepartment;
+    $instanceCareer->organizationName = $request->careerOrganizationName;
+    $instanceCareer->organizationDepartment = $request->careerOrganizationDepartment;
+    $instanceCareer->universityName = $request->careerUniversityName;
+    $instanceCareer->universityDepartment = $request->careerUniversityDepartment;
+    $instanceCareer->careerOrganizationTypeId = $request->careerOrganizationType;
+    $instanceCareer->personId = $personId;
+    $instanceCareer->division = $request->careerDivision;
     $instanceCareer->save();
     
     $instanceUserLogin = new UserLogin;
-    $instanceUserLogin->login = $request->;
-    $instanceUserLogin->password = $request->;
-    $instanceUserLogin->isDisabled = $request->;
-    $instanceUserLogin->personId = $request->;
+    $instanceUserLogin->login = $personCode;
+    $instanceUserLogin->password = (new BcryptHasher)->make($personCode);
+    $instanceUserLogin->isDisabled = 0;
+    $instanceUserLogin->personId = $personId;
     $instanceUserLogin->save();
+    $UserLoginId = $instanceUserLogin->id;
     
     $instanceUserLoginUserRole = new UserLoginUserRole;
-    $instanceUserLoginUserRole->userLoginId = $request->;
-    $instanceUserLoginUserRole->userRoleId = $request->;
+    $instanceUserLoginUserRole->userLoginId = $UserLoginId;
+    $instanceUserLoginUserRole->userRoleId = 2;
     $instanceUserLoginUserRole->save();
 
-    return response()->json($request);
+    return response()->json($this->response);
   }
 
 }
