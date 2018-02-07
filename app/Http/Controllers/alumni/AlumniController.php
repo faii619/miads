6dd712efbba4a86140bee67d7ec7490fd317fb4a
faii619 <faiifylmus@gmail.com>
2018-02-07 11:51:42 +0700

    edit function edit for AlumniController

--- app/Http/Controllers/alumni/AlumniController.php
+++ app/Http/Controllers/alumni/AlumniController.php
@@ -11,7 +11,6 @@ use App\Models\user_login_user_role\UserLoginUserRole;
 use App\Models\career\Career;
 use App\Models\program_participant\ProgramParticipant;
 use Illuminate\Http\Request;
-use Illuminate\Hashing\BcryptHasher;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class AlumniController extends BaseController
@@ -94,14 +93,16 @@ class AlumniController extends BaseController
   public function create(Request $request)
   {
     $personCode = $request->code;
+    $imageName = (!empty($request->imageName) && $request->imageName != '') ? $request->imageName : 'default.jpg' ;
+    $imageSize = (!empty($request->imageSize) && $request->imageSize != '') ? $request->imageSize : '0' ;
 
     $instanceFile = new File;
-    $instanceFile->fileName = $request->imageName;
-    $instanceFile->fileSize = $request->imageSize;
+    $instanceFile->fileName = $imageName;
+    $instanceFile->fileSize = $imageSize;
     $instanceFile->save();
     $fileId = $instanceFile->id;
     
-    $instanceHomeAddress = new address;
+    $instanceHomeAddress = new Address;
     $instanceHomeAddress->streetAddress = $request->homeStreetAddress;
     $instanceHomeAddress->city = $request->homeCity;
     $instanceHomeAddress->province = $request->homeProvince;
@@ -113,7 +114,7 @@ class AlumniController extends BaseController
     $instanceHomeAddress->save();
     $homeId = $instanceHomeAddress->id;
     
-    $instanceOfficeAddress = new address;
+    $instanceOfficeAddress = new Address;
     $instanceOfficeAddress->streetAddress = $request->officeStreetAddress;
     $instanceOfficeAddress->city = $request->officeCity;
     $instanceOfficeAddress->province = $request->officeProvince;
@@ -162,7 +163,7 @@ class AlumniController extends BaseController
     
     $instanceUserLogin = new UserLogin;
     $instanceUserLogin->login = $personCode;
-    $instanceUserLogin->password = (new BcryptHasher)->make($personCode);
+    $instanceUserLogin->password = md5($personCode);
     $instanceUserLogin->isDisabled = 0;
     $instanceUserLogin->personId = $personId;
     $instanceUserLogin->save();
@@ -179,84 +180,76 @@ class AlumniController extends BaseController
   public function edit(Request $request)
   {
     $personId = $request->id;
-    // $personCode = $request->code;
+    $personCode = $request->code;
+    $fileId = $request->fileId;
+    $homeId = $request->homeId;
+    $officeId = $request->officeId;
 
-    // $instanceFile = new File;
-    // $instanceFile->fileName = $request->imageName;
-    // $instanceFile->fileSize = $request->imageSize;
-    // $instanceFile->save();
-    // $fileId = $instanceFile->id;
+    $resultPerson = Person::find($personId);
+    $resultPerson->personTitleId = $request->title;
+    $resultPerson->name = $request->name;
+    $resultPerson->birthDate = $request->birthday;
+    $resultPerson->email = $request->email;
+    $resultPerson->otherEmails = $request->otherEmails;
+    $resultPerson->photoFileId = $fileId;
+    $resultPerson->homeAddressId = $homeId;
+    $resultPerson->officeAddressId = $officeId;
+    $resultPerson->isPreferOfficeContact = $request->ContactAddress;
+    $resultPerson->gender = $request->gender;
+    $resultPerson->nationalityAddressCountryId = $request->nationCountry;
+    $resultPerson->save();
+
+    $resultFile = File::find($fileId);
+    $resultFile->fileName = $request->imageName;
+    $resultFile->fileSize = $request->imageSize;
+    $resultFile->save();
     
-    // $instanceHomeAddress = new address;
-    // $instanceHomeAddress->streetAddress = $request->homeStreetAddress;
-    // $instanceHomeAddress->city = $request->homeCity;
-    // $instanceHomeAddress->province = $request->homeProvince;
-    // $instanceHomeAddress->addressCountryId = $request->homeCountry;
-    // $instanceHomeAddress->zip = $request->homeZip;
-    // $instanceHomeAddress->tel = $request->homeTel;
-    // $instanceHomeAddress->fax = $request->homeFax;
-    // $instanceHomeAddress->mobile = $request->homeMobile;
-    // $instanceHomeAddress->save();
-    // $homeId = $instanceHomeAddress->id;
+    Alumni::where([['personId', '=', $personId]])
+            ->update([
+                'code' => $personCode
+              ]);
+
+    UserLogin::where([['personId', '=', $personId]])
+              ->update([
+                  'login' => $personCode
+                ]);
     
-    // $instanceOfficeAddress = new address;
-    // $instanceOfficeAddress->streetAddress = $request->officeStreetAddress;
-    // $instanceOfficeAddress->city = $request->officeCity;
-    // $instanceOfficeAddress->province = $request->officeProvince;
-    // $instanceOfficeAddress->addressCountryId = $request->officeCountry;
-    // $instanceOfficeAddress->zip = $request->officeZip;
-    // $instanceOfficeAddress->tel = $request->officeTel;
-    // $instanceOfficeAddress->fax = $request->officeFax;
-    // $instanceOfficeAddress->mobile = $request->officeMobile;
-    // $instanceOfficeAddress->save();
-    // $officeId = $instanceOfficeAddress->id;
+    $resultHomeAddress = Address::find($homeId);
+    $resultHomeAddress->streetAddress = $request->homeStreetAddress;
+    $resultHomeAddress->city = $request->homeCity;
+    $resultHomeAddress->province = $request->homeProvince;
+    $resultHomeAddress->addressCountryId = $request->homeCountry;
+    $resultHomeAddress->zip = $request->homeZip;
+    $resultHomeAddress->tel = $request->homeTel;
+    $resultHomeAddress->fax = $request->homeFax;
+    $resultHomeAddress->mobile = $request->homeMobile;
+    $resultHomeAddress->save();
     
-    // $instancePerson = new Person;
-    // $instancePerson->personTitleId = $request->title;
-    // $instancePerson->name = $request->name;
-    // $instancePerson->birthDate = $request->birthday;
-    // $instancePerson->email = $request->email;
-    // $instancePerson->otherEmails = $request->otherEmails;
-    // $instancePerson->photoFileId = $fileId;
-    // $instancePerson->homeAddressId = $homeId;
-    // $instancePerson->officeAddressId = $officeId;
-    // $instancePerson->isPreferOfficeContact = $request->ContactAddress;
-    // $instancePerson->gender = $request->gender;
-    // $instancePerson->nationalityAddressCountryId = $request->nationCountry;
-    // $instancePerson->save();
+    $resultOfficeAddress = Address::find($officeId);
+    $resultOfficeAddress->streetAddress = $request->officeStreetAddress;
+    $resultOfficeAddress->city = $request->officeCity;
+    $resultOfficeAddress->province = $request->officeProvince;
+    $resultOfficeAddress->addressCountryId = $request->officeCountry;
+    $resultOfficeAddress->zip = $request->officeZip;
+    $resultOfficeAddress->tel = $request->officeTel;
+    $resultOfficeAddress->fax = $request->officeFax;
+    $resultOfficeAddress->mobile = $request->officeMobile;
+    $resultOfficeAddress->save();
     
-    // $instanceAlumni = new Alumni;
-    // $instanceAlumni->code = $personCode;
-    // $instanceAlumni->personId = $personId;
-    // $instanceAlumni->save();
-    
-    // $instanceCareer = new Career;
-    // $instanceCareer->position = $request->careerPosition;
-    // $instanceCareer->startYear = $request->careerStartYear;
-    // $instanceCareer->areaOfExpertise = $request->careerExpertise;
-    // $instanceCareer->govMinistryName = $request->careerMinistry;
-    // $instanceCareer->govDepartmentName = $request->careerDepartment;
-    // $instanceCareer->organizationName = $request->careerOrganizationName;
-    // $instanceCareer->organizationDepartment = $request->careerOrganizationDepartment;
-    // $instanceCareer->universityName = $request->careerUniversityName;
-    // $instanceCareer->universityDepartment = $request->careerUniversityDepartment;
-    // $instanceCareer->careerOrganizationTypeId = $request->careerOrganizationType;
-    // $instanceCareer->personId = $personId;
-    // $instanceCareer->division = $request->careerDivision;
-    // $instanceCareer->save();
-    
-    // $instanceUserLogin = new UserLogin;
-    // $instanceUserLogin->login = $personCode;
-    // $instanceUserLogin->password = (new BcryptHasher)->make($personCode);
-    // $instanceUserLogin->isDisabled = 0;
-    // $instanceUserLogin->personId = $personId;
-    // $instanceUserLogin->save();
-    // $UserLoginId = $instanceUserLogin->id;
-    
-    // $instanceUserLoginUserRole = new UserLoginUserRole;
-    // $instanceUserLoginUserRole->userLoginId = $UserLoginId;
-    // $instanceUserLoginUserRole->userRoleId = 2;
-    // $instanceUserLoginUserRole->save();
+    Career::where([['personId', '=', $personId]])
+            ->update([
+              'position' => $request->careerPosition
+              , 'startYear' => $request->careerStartYear
+              , 'areaOfExpertise' => $request->careerExpertise
+              , 'govMinistryName' => $request->careerMinistry
+              , 'govDepartmentName' => $request->careerDepartment
+              , 'organizationName' => $request->careerOrganizationName
+              , 'organizationDepartment' => $request->careerOrganizationDepartment
+              , 'universityName' => $request->careerUniversityName
+              , 'universityDepartment' => $request->careerUniversityDepartment
+              , 'careerOrganizationTypeId' => $request->careerOrganizationType
+              , 'division' => $request->careerDivision
+            ]);
 
     return response()->json($this->response);
   }
