e5c44c2aaa7048509ee0e3dba68d2d7cd5a19a7d
geidtiphong <geidtiphong@gmail.com>
2018-02-08 08:37:37 +0700

    edit ministry data OK

--- app/Http/Controllers/authority_control/MinistryController.php
+++ app/Http/Controllers/authority_control/MinistryController.php
@@ -8,43 +8,52 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class MinistryController extends BaseController
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+      //
+  }
 
-    private $response = array('status' => 1, 'message' => 'success');
+  private $response = array('status' => 1, 'message' => 'success');
 
-    public function ministry() {
-        // $results = Career::where('status', 1)->get();
-        $results = Career::where('status', 1)->take(200)->get();
-        
-        return response()->json($results);
-    }
+  public function ministry() {
+    // $results = Career::where('status', 1)->get();
+    $results = Career::where('status', 1)->take(200)->get();
+    
+    return response()->json($results);
+  }
 
-    public function delete($id) {
-        $results = Career::find($id);
-        $results->status = 0;
-        $results->save();
-        return response()->json($this->response);
-      }
+  public function create(Request $request) {
+    $result = new Career;
+    $result->govMinistryName = $request->govMinistryName;
+    $result->save();
+    return response()->json($this->response); 
+  }
 
-    public function create(Request $request){
-        $result = new Career;
-        $result->govMinistryName = $request->govMinistryName;
-        $result->save();
-        return response()->json($this->response); 
-      }
-      public function create_department(Request $request){
-        $result = new Career;
-        $result->govDepartmentName = $request->govDepartmentName;
-        $result->save();
-        return response()->json($this->response); 
-      }
+  public function edit(Request $request) {
+    $results = Career::find($request->id);
+    $results->govMinistryName = $request->govMinistryName;
+    $results->status = 1;
+    $results->save();
+    return response()->json($this->response);
+  }
+
+  public function delete($id) {
+    $results = Career::find($id);
+    $results->status = 0;
+    $results->save();
+    return response()->json($this->response);
+  }
+
+  public function create_department(Request $request) {
+    $result = new Career;
+    $result->govDepartmentName = $request->govDepartmentName;
+    $result->save();
+    return response()->json($this->response); 
+  }
 
 }
