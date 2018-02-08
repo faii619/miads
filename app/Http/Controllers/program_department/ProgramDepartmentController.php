f4e1730f82324eef3d05a82544f533fbc7f40fb7
iviooThong <kitipot2538@gmail.com>
2018-02-08 15:32:11 +0700

    edit ProgramDepartmentController

--- app/Http/Controllers/program_department/ProgramDepartmentController.php
+++ app/Http/Controllers/program_department/ProgramDepartmentController.php
@@ -8,39 +8,43 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class ProgramDepartmentController extends BaseController
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+      //
+  }
 
-    private $response = array('message' => 'success');
+  private $response = array('status' => 1, 'message' => 'success');  
 
-    public function program_department() {
-      $results = ProgramDepartment::where('status', 1)->get();
-      return response()->json($results);
-    }
+  public function program_department() {
+    // $results = ProgramDepartment::where('status', 1)->get();
+    $results = ProgramDepartment::where('status', 1)->take(200)->get();
+    return response()->json($results);
+  }
 
-    public function create(Request $request) {
-      $results = new ProgramDepartment;
-      $results->caption = $request->caption;
-      $results->save();
-      return response()->json($this->response);
-    }
+  public function create(Request $request) {
+    $results = new ProgramDepartment;
+    $results->caption = $request->caption;
+    $results->save();
+    return response()->json($this->response);
+  }
 
-    public function edit(Request $request) {
-      $results = ProgramDepartment::find($request->id);
-      $results->caption = $request->caption;
-      $results->save();
-      return response()->json($this->response);
-    }
+  public function edit(Request $request) {
+    $results = ProgramDepartment::find($request->id);
+    $results->caption = $request->caption;
+    $results->status = 1;
+    $results->save();
+    return response()->json($this->response);
+  }
 
-    public function delete($id) {
-      ProgramDepartment::where('id', $id)->delete();
-      return response()->json($this->response);
-    }
+  public function delete($id) {
+    $results = ProgramDepartment::find($id);
+    $results->status = 0;
+    $results->save();
+    return response()->json($this->response);
+  }
 }
