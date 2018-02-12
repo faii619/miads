8a55da5795a150129277e7eddc948598612858ba
geidtiphong <geidtiphong@gmail.com>
2018-02-12 10:43:00 +0700

    formatter code Country Class

--- app/Http/Controllers/country/CountryController.php
+++ app/Http/Controllers/country/CountryController.php
@@ -20,33 +20,37 @@ class CountryController extends BaseController
 
   private $response = array('status' => 1, 'message' => 'success');
       
-  public function country() {
+  public function country() 
+  {
     // $results = Career::where('status', 1)->get();
     $results = Country::where('status', 1)->take(200)->get();
     return response()->json($results);
   }
     
-    public function create(Request $request){
-        $result = new Country;
-        $result->caption = $request->caption;
-        // $result->ordinal = $request->ordinal;
-        $result->save();
-        return response()->json($this->response); 
-      }
+    public function create(Request $request)
+    {
+      $result = new Country;
+      $result->caption = $request->caption;
+      // $result->ordinal = $request->ordinal;
+      $result->save();
+      return response()->json($this->response); 
+    }
 
-      public function edit(Request $request){
-        $result = Country::find($request->id);
-        $result->caption = $request->caption;
-        // $result->ordinal = $request->ordinal;
-        $result->save();
-        return response()->json($this->response); 
-      }
-      
-      public function delete($id) {
-        $results = Country::find($id);
-        $results->status = 0;
-        $results->save();
-        return response()->json($this->response);
-      }
+    public function edit(Request $request)
+    {
+      $result = Country::find($request->id);
+      $result->caption = $request->caption;
+      // $result->ordinal = $request->ordinal;
+      $result->save();
+      return response()->json($this->response); 
+    }
+    
+    public function delete($id) 
+    {
+      $results = Country::find($id);
+      $results->status = 0;
+      $results->save();
+      return response()->json($this->response);
+    }
    
 }
