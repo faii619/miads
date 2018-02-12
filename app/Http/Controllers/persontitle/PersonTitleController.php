28e540c8aac512cadf8be7e520f110d59f4cecec
faii619 <faiifylmus@gmail.com>
2018-02-12 15:01:01 +0700

    create controller and models about persontitle

---
+++ app/Http/Controllers/persontitle/PersonTitleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\persontitle;
+
+use App\Models\persontitle\PersonTitle;
+use Illuminate\Http\Request;
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+class PersonTitleController extends BaseController {
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+      //
+  }
+  
+}
