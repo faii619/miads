8eda3f0e45a34a760553561066523d9936db5afe
geidtiphong <geidtiphong@gmail.com>
2018-02-12 15:37:32 +0700

    get image url OK

--- app/Http/Controllers/country/CountryController.php
+++ app/Http/Controllers/country/CountryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\country;
 
 use App\Models\country\Country;
 use App\Http\Controllers\UploadController;
+use App\Http\Controllers\ImageController;
 use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
@@ -20,48 +21,49 @@ class CountryController extends BaseController
     }
 
   private $response = array('status' => 1, 'message' => 'success');
-  private $path = 'images/country';
+  private $path = 'images/country/';
 
   public function country() 
   {
-    // $results = Career::where('status', 1)->get();
     $results = Country::where('status', 1)->orderBy('caption', 'asc')->take(200)->get();
+    $images = new ImageController();
+    $results = $images->getImagesUrl($results, $this->path, 'flagImage');
     return response()->json($results);
   }
     
-    public function create(Request $request)
-    {
-      $upload = new UploadController();
-      $image = $upload->setImage($request, $this->path);
-      
-      $result = new Country;
-      $result->caption = $request->caption;
-      $result->ordinal = $request->ordinal;
-      $result->flagImage = $image;
-
-      $result->save();
-      return response()->json($this->response);
-    }
-
-    public function edit(Request $request)
-    {
-      $upload = new UploadController();
-      $image = $upload->setImage($request, $this->path);
-
-      $result = Country::find($request->id);
-      $result->caption = $request->caption;
-      $result->flagImage = $image;
-
-      $result->save();
-      return response()->json($this->response);
-    }
+  public function create(Request $request)
+  {
+    $upload = new UploadController();
+    $image = $upload->setImage($request, $this->path);
     
-    public function delete($id) 
-    {
-      $results = Country::find($id);
-      $results->status = 0;
-      $results->save();
-      return response()->json($this->response);
-    }
+    $result = new Country;
+    $result->caption = $request->caption;
+    $result->ordinal = $request->ordinal;
+    $result->flagImage = $image;
+
+    $result->save();
+    return response()->json($this->response);
+  }
+
+  public function edit(Request $request)
+  {
+    $upload = new UploadController();
+    $image = $upload->setImage($request, $this->path);
+
+    $result = Country::find($request->id);
+    $result->caption = $request->caption;
+    $result->flagImage = $image;
+
+    $result->save();
+    return response()->json($this->response);
+  }
+  
+  public function delete($id) 
+  {
+    $results = Country::find($id);
+    $results->status = 0;
+    $results->save();
+    return response()->json($this->response);
+  }
    
 }
