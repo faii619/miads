a70b57a3f01988929525b006952f89f87d7524c7
faii619 <faiifylmus@gmail.com>
2018-02-13 11:31:58 +0700

    create models:careerorganizationtype

---
+++ app/Models/careerorganizationtype/CareerOrganizationType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\careerorganizationtype;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CareerOrganizationType extends Model
+{
+  protected $table = "CareerOrganizationType";
+  public $timestamps = false;
+}
+
