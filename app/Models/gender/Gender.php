6576e79ec4fac1c5f4a3de8dd2f403262c87951d
faii619 <faiifylmus@gmail.com>
2018-02-13 10:08:06 +0700

    create models Gender

---
+++ app/Models/gender/Gender.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\gender;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Gender extends Model
+{
+  protected $table = "Gender";
+  public $timestamps = false;
+}
+
