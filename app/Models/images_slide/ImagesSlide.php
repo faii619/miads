5deba8b28f639c39299546c09d59dca99adc301f
faii619 <faiifylmus@gmail.com>
2018-03-08 15:44:30 +0700

    create models images_slide, news_send_to and folder images slide

---
+++ app/Models/images_slide/ImagesSlide.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\images_slide;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ImagesSlide extends Model
+{
+  protected $table = "ImagesSlide";
+  public $timestamps = false;
+}
---
+++ app/Models/news_send_to/NewsSendTo.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\news_send_to;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NewsSendTo extends Model {
+  protected $table = "NewsSendTo";
+  public $timestamps = false;
+}
