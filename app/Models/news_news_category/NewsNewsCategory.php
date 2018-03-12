a2c584c8a4a05883b6a7bfda23abf3c784ce9863
faii619 <faiifylmus@gmail.com>
2018-03-12 15:51:37 +0700

    create model News_NewsCategory

---
+++ app/Models/news_news_category/NewsNewsCategory.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\news_news_category;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NewsNewsCategory extends Model {
+  protected $table = "News_NewsCategory";
+  public $timestamps = false;
+}
