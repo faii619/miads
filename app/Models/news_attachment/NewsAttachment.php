de0fee725bca8e833fba2fe545ba22ccf7c41b4c
faii619 <faiifylmus@gmail.com>
2018-03-16 13:52:25 +0700

    create models NewsAttachent

---
+++ app/Models/news_attachment/NewsAttachment.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\news_attachment;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NewsAttachment extends Model {
+  protected $table = "NewsAttachment";
+  public $timestamps = false;
+}
